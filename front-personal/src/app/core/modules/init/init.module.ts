57d85dfe5dd8cf9df64e456a6eb5488726948a0d
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-08-25 11:31:42 -0400

    added header

--- front-personal/src/app/core/modules/init/init.module.ts
+++ front-personal/src/app/core/modules/init/init.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { InitRoutingModule } from './init-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [FooterComponent, NavbarComponent],
+  declarations: [FooterComponent, NavbarComponent, HeaderComponent],
   imports: [CommonModule, InitRoutingModule],
-  exports: [FooterComponent],
+  exports: [FooterComponent, NavbarComponent, HeaderComponent],
 })
 export class InitModule {}
