ff6dd2bbf0b69a87c9192493829426c7c3140421
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-08-27 11:28:15 -0400

    added firebase auth and init with fireabase, adding sign in stuff

--- front-personal/src/app/app.module.ts
+++ front-personal/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitModule } from './core/modules/init/init.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, InitModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    InitModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ front-personal/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'sheng-persona',
+    appId: '1:814036793736:web:158fb1df448ce3a2a3a23d',
+    storageBucket: 'sheng-persona.appspot.com',
+    apiKey: 'AIzaSyCKELF6JMGiqsa1eoNn1WoEMpNlUY67LMA',
+    authDomain: 'sheng-persona.firebaseapp.com',
+    messagingSenderId: '814036793736',
+    measurementId: 'G-GLBVC9MTG1',
+  },
+};
