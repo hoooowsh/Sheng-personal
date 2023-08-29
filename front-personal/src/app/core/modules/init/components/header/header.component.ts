4ed06b09192e0ad48324d37fc1b5522a8bec1465
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-08-28 21:42:54 -0400

    added signin popup

--- front-personal/src/app/core/modules/init/components/header/header.component.ts
+++ front-personal/src/app/core/modules/init/components/header/header.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService) {}
 
+  signInWithGoogle() {
+    this.authService
+      .googleSignIn()
+      .then((result) => {
+        console.log('Successfully signed in with Google:', result);
+        // Handle the signed-in user here.
+      })
+      .catch((error) => {
+        console.error('Error during sign in:', error);
+      });
+  }
 }
--- front-personal/src/app/core/modules/init/services/auth.service.ts
+++ front-personal/src/app/core/modules/init/services/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  // getAuth,
+  // User,
+  // signInWithEmailAndPassword,
+  // signOut,
+  // createUserWithEmailAndPassword,
+} from '@angular/fire/auth';
+import { FirebaseApp } from '@angular/fire/app';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  // auth: Auth;
+  // user: User | null | undefined;
 
-  constructor() { }
+  constructor(private afApp: FirebaseApp, private auth: Auth) {
+    // this.auth = getAuth(this.afApp);
+  }
+
+  async googleSignIn() {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(this.auth, provider);
+      return result;
+    } catch (error) {
+      console.error('Error during Google sign in:', error);
+      throw error;
+    }
+  }
+
+  // Get current user
+  // getCurrentUser(): User | null | undefined {
+  //   return this.user;
+  // }
 }
