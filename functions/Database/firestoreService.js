cf85dc7b03a9129b055242f48b7dc617a3582841
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-09-02 14:25:48 -0400

    added comment and updated

--- functions/Database/firestoreService.js
+++ functions/Database/firestoreService.js
@@ -1,23 +1,41 @@
-const admin = require("firebase-admin");
-const db = admin.firestore();
+const { db } = require("../Config/firebase");
+// const db = admin.firestore();
 
 module.exports = {
-  // get data
+  /**
+   * First level get document helper function for general use cases
+   * @param {String} collection - the collection name, "Users" for exmaple
+   * @param {String} documentId - document id, search key in database
+   * @returns An object of doc content
+   */
   async getDocument(collection, documentId) {
     const doc = await db.collection(collection).doc(documentId).get();
     return doc.exists ? doc.data() : null;
   },
 
-  // update data
+  /**
+   * First level update document content helper function for general use cases
+   * @param {String} collection - the collection name, "Users" for exmaple
+   * @param {String} documentId - document id, search key in database
+   * @param {Object} data - data object for updating database
+   * @returns None
+   */
   async setDocument(collection, documentId, data) {
     await db.collection(collection).doc(documentId).set(data);
+    return;
   },
 
-  // add data
-  async addDocument(collection, data, id = null) {
+  /**
+   * First level add document content helper function for general use cases
+   * @param {String} collection - the collection name, "Users" for exmaple
+   * @param {String} documentId - document id, search key in databasem. Null if not given
+   * @param {object} data - data object for adding to database
+   * @returns The id of the new created document
+   */
+  async addDocument(collection, documentId = null, data) {
     let docRef;
-    if (id) {
-      docRef = await db.collection(collection).doc(id);
+    if (documentId) {
+      docRef = await db.collection(collection).doc(documentId);
       await docRef.set(data);
     } else {
       docRef = await db.collection(collection).add(data);
@@ -25,44 +43,46 @@ module.exports = {
     return docRef.id;
   },
 
-  // delete data
+  /**
+   * First level delete document content helper function for general use cases
+   * @param {String} collection - the collection name, "Users" for exmaple
+   * @param {String} documentId - document id, search key in databasem
+   * @returns None
+   */
   async deleteDocument(collection, documentId) {
     await db.collection(collection).doc(documentId).delete();
     return;
   },
 
-  async editFirstLevelArray(
-    collection,
-    documentId,
-    field,
-    value,
-    operation = "set"
-  ) {
-    const docRef = db.collection(collection).doc(documentId);
-    let updateData;
-
-    // do the operation
-    switch (operation) {
-      case "arrayRemove":
-        updateData = { [field]: admin.firestore.FieldValue.arrayRemove(value) };
-        break;
-      case "arrayAdd":
-        updateData = { [field]: admin.firestore.FieldValue.increment(value) };
-        break;
-      default:
-        updateData = { [field]: value };
-        break;
-    }
-    await docRef.update(updateData);
-  },
-
-  // give query to get data that satisfy the constriant
-  // queryCollection('Users', 'age', '>=', 21)
+  /**
+   * First level query helper function to get data that satisfies the constriant
+   * @param {String} collection - the collection name, "Users" for exmaple
+   * @param {String} field - the field name for doing the comparison
+   * @param {String} operator - the operator, > < =
+   * @param {Number} value - value to compare with
+   * @returns List of objects that satisfy the constriant
+   */
   async queryCollection(collection, field, operator, value) {
     const snapshot = await db
       .collection(collection)
       .where(field, operator, value)
       .get();
     return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  },
+
+  /**
+   * Seconde level get document helper function for general use cases
+   * @param {String} collection - the collection name, "Thoughts" for exmaple
+   * @param {String} documentId - document id, search key in database. Thoughts ID for example
+   * @returns An object of doc content
+   */
+  async getDocumentL2(collectionL1, documentIdL1, collectionL2, documentIdL2) {
+    const doc = await db
+      .collection(collectionL1)
+      .doc(documentIdL1)
+      .collection(collectionL2)
+      .doc(documentIdL2)
+      .get();
+    return doc.exists ? doc.data() : null;
   },
 };
