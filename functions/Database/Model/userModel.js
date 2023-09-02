3e4a382ccb54e52d7c49bc2a7912449463784311
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-09-02 15:45:48 -0400

    update user model comments

--- functions/Database/Model/userModel.js
+++ functions/Database/Model/userModel.js
@@ -1,32 +1,39 @@
 class User {
-  // User schema constructor
-  constructor(id, name, email, isAdmin, thoughts = []) {
+  /**
+   * User model constructor
+   * @param {String} id - user id, should get from firebase auth
+   * @param {String} name - user name, should get it from google OAuth
+   * @param {String} email - user name, should get from google OAuth
+   * @param {Boolean} isAdmin - to tell if the user is Admin or not
+   */
+  constructor(id, name, email, isAdmin) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.isAdmin = isAdmin;
-    this.thoughts = thoughts;
   }
 
-  // using this to return data, get user data using id
+  // Helper function to return data, get user data using id
+  /**
+   * Helper function to get data from database for User
+   * @param {String} userId - search key for user, used in both firebase Auth and firestore
+   * @param {Object} data - user object get from database
+   * @returns User object
+   */
   static fromData(userId, data) {
     console.log(userId, data);
-    return new User(
-      userId,
-      data.name,
-      data.email,
-      data.isAdmin,
-      data.thoughts || [] // Default to empty array if thoughts are not available
-    );
+    return new User(userId, data.name, data.email, data.isAdmin);
   }
 
-  // used for creating database entry
+  /**
+   * Helper function to create user instance in database
+   * @returns An object that has all docs for a user instance in database
+   */
   toData() {
     return {
       name: this.name,
       email: this.email,
       isAdmin: this.isAdmin,
-      thoughts: this.thoughts,
     };
   }
 }
