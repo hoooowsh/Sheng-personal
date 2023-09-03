8f95a602c2147cef67b5df6f1732b6fe8f008504
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-09-02 20:14:57 -0400

    added comments

--- functions/Helper/admin.js
+++ functions/Helper/admin.js
@@ -1,3 +1,8 @@
+/**
+ * Helper function to tell if the email is admin email or not
+ * @param {String} email - email
+ * @returns boolean
+ */
 const isAdmin = (email) => {
   if (email === process.env.ADMIN_EMAIL) {
     return true;
