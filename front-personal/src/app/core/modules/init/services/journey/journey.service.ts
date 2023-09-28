51d5e2d17b784235723673da8252c4c110879d28
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-09-27 22:17:34 -0400

    added journey and leetcode service

--- front-personal/src/app/core/modules/init/services/journey/journey.service.ts
+++ front-personal/src/app/core/modules/init/services/journey/journey.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ObjForList } from '../../Models/ObjForList.model';
+import { Journey } from '../../Models/Journey.model';
+import { AuthService } from '../auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JourneyService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  constructor() { }
+  getAllJourney(): Observable<ObjForList[]> {
+    return this.http
+      .get<any>(`${environment.backendUrl}/journey/journeyList`)
+      .pipe(map((response) => response.journeyList));
+  }
+
+  getOneJourney(journeyId: string): Observable<Journey> {
+    return this.http
+      .get<any>(`${environment.backendUrl}/journey/id/${journeyId}`)
+      .pipe(map((response) => response.journey));
+  }
+
+  async addOneJourney(title: string, content: string) {
+    const email = await this.authService.getUserEmail();
+    if (email !== environment.adminEmail) {
+      throw new Error('User not authenticated');
+    }
+    const token = await this.authService.getUserToken();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      }),
+    };
+    const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+
+    return this.http.post<any>(
+      `${environment.backendUrl}/journey/add`,
+      {
+        title: title,
+        content: content,
+        date: currentTimeInSeconds,
+      },
+      httpOptions
+    );
+  }
+
+  async deleteOneJourney(journeyId: string) {
+    const email = await this.authService.getUserEmail();
+    if (email !== environment.adminEmail) {
+      throw new Error('User not authenticated');
+    }
+    const token = await this.authService.getUserToken();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      }),
+    };
+    return this.http.post<any>(
+      `${environment.backendUrl}/journey/delete`,
+      {
+        journeyId: journeyId,
+      },
+      httpOptions
+    );
+  }
 }
--- front-personal/src/app/core/modules/init/services/leetcode/leetcode.service.ts
+++ front-personal/src/app/core/modules/init/services/leetcode/leetcode.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ObjForList } from '../../Models/ObjForList.model';
+import { Leetcode } from '../../Models/Leetcode.model';
+import { AuthService } from '../auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LeetcodeService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  constructor() { }
+  getAllLeetcode(): Observable<ObjForList[]> {
+    return this.http
+      .get<any>(`${environment.backendUrl}/leetcode/leetcodeList`)
+      .pipe(map((response) => response.leetcodeList));
+  }
+
+  getOneLeetcode(leetcodeId: string): Observable<Leetcode> {
+    return this.http
+      .get<any>(`${environment.backendUrl}/leetcode/id/${leetcodeId}`)
+      .pipe(map((response) => response.leetcode));
+  }
+
+  async addOneLeetcode(title: string, topic: string, content: string) {
+    const email = await this.authService.getUserEmail();
+    if (email !== environment.adminEmail) {
+      throw new Error('User not authenticated');
+    }
+    const token = await this.authService.getUserToken();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      }),
+    };
+    const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+
+    return this.http.post<any>(
+      `${environment.backendUrl}/leetcode/add`,
+      {
+        title: title,
+        topic: topic,
+        content: content,
+        date: currentTimeInSeconds,
+      },
+      httpOptions
+    );
+  }
+
+  async deleteOneLeetcode(leetcodeId: string) {
+    const email = await this.authService.getUserEmail();
+    if (email !== environment.adminEmail) {
+      throw new Error('User not authenticated');
+    }
+    const token = await this.authService.getUserToken();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      }),
+    };
+    return this.http.post<any>(
+      `${environment.backendUrl}/leetcode/delete`,
+      {
+        leetcodeId: leetcodeId,
+      },
+      httpOptions
+    );
+  }
 }
